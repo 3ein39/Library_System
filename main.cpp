344d87443bae3b32790aaa021ee0646d9823cf9b
HusseinHany <hhany3997@gmail.com>
2022-04-29 16:33:40 +0200

    feat: add_book

--- main.cpp
+++ main.cpp
@@ -13,8 +13,10 @@ struct Books {
 
     vector<pair<string, string>> borrowed_books; // user name .. book name
 };
-
 Books books[MAX_BOOKS + 1]; // from 1 to 10 distinct books
+bool cmp_books_by_id(Books x, Books y){
+    return x.id < y.id;
+}
 
 struct Users {
     int id {};
@@ -22,9 +24,7 @@ struct Users {
     int borrowed_books_ids[];
 
 };
-
 Users users[MAX_USERS + 1]; // from 1 to 10 distinct users
-
 
 int menu() {
     int choice = -1;
@@ -50,14 +50,39 @@ int menu() {
     return choice;
 }
 
+bool add_book() {
+    int pos {0};
+    while(books[pos].id != 0){ // gets next free position in books array;
+        if (pos == 10){ // reaching maximum book number handled
+            cout << "Maximum books reached [10].\n"
+                << "YOU CAN'T ADD MORE!\n";
+            return false;
+        }
+        pos++;
+    }
+    cout << "Enter book info: id & name & total quantity: ";
+    cin >> books[pos].id >> books[pos].name >> books[pos].total_quantity;
+    return true;
+}
 
-
+void print_library_by_id(){
+    sort(books, books + MAX_BOOKS + 1, cmp_books_by_id);
+    for (auto book: books) {
+        if (book.id != 0) {
+            cout << "Id = " << book.id << " name = " << book.name
+                 << " total_quantity = " << book.total_quantity
+                 << " total_borrowed = " << book.total_borrowed << endl;
+        }
+    }
+}
 
 void system() {
     while (true){
         int choice = menu();
         if (choice == 1)
-            int test;
+            add_book();
+        else if (choice == 4)
+            print_library_by_id();
         else if (choice == 10)
             break;
     }
